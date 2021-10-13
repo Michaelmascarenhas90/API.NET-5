ce5c6f9ed2eef3e57516a85d8dd3c36901f37c5f
Michaelmascarenhas90 <michaelmascarenhastrybe@gmail.com>
2021-10-13 10:12:14 -0300

    configuração inicial API

---
+++ models/Pessoa.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Net.Mime;
+using System.ComponentModel.DataAnnotations;
+
+namespace DOTNETAPI.models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int CodUser { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Senha { get; set; }
+
+        public int FunctionUser { get; set; }
+
+        public int CdProtheus { get; set; }
+
+        public string Email { get; set; }
+
+
+    }
+}
